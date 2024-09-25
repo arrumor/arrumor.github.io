9a434bec09e4ce4420fda32e8688a0a34326211d
Celio Arga Rumahorbo <148985563+arrumor@users.noreply.github.com>
2024-09-25 16:23:55 +0800

    Update script.js

--- POSTTEST_3/script.js
+++ POSTTEST_3/script.js
@@ -24,8 +24,6 @@ const menu = document.getElementById('menu');
 hamburger.addEventListener('click', function() {
     if (menu.style.display === 'block') {
         menu.style.display = 'none';
-    } else {
-        menu.style.display = 'block';
     }
 });
 
